be93837468b5cccb3b0184cfaa4ef7493ccdde1b
lakshman mallidi <lakshman.mallidi@gmail.com>
2019-10-09 11:51:37 +0530

    Update manageDatabase.py

--- manageDatabase.py
+++ manageDatabase.py
@@ -1,30 +1,10 @@
-import mysql.connector
+from database import connection
 import sys
 import serial
 from getpass import getpass
 try:
     com = input("Enter Rfid detector com port(baudrate:9600):")
-    ipaddress = input("Enter your database server host address:")
-    user_db = input("Enter username:")
-    password_db = getpass("Enter password:")
-    choice = input("Enter Y to create new database or N to use existing database:")
-    db_name = None
-    if(choice == "Y" or choice=="y"):
-        db_name = input("Enter database name:")
-        c = mysql.connector.connect(host=ipaddress,
-                                   user=user_db,
-                                   passwd=password_db)
-        cur = c.cursor()
-        cur.execute('CREATE DATABASE '+db_name)
-        c.commit()
-        c.close()
-    elif(choice=="N" or choice=="n"):
-        db_name = input("Enter database name:")
-    conn = mysql.connector.connect(host=ipaddress,
-                                   user=user_db,
-                                   passwd=password_db,
-                                   db=db_name)
-    c = conn.cursor()
+    c,conn = connection()
     while True:
         choice = int(input("\n-------------------------\nklef log book\n-------------------------\n1.Create New Room\n2.Delete Room\n3.Show all Room\n4.Add new Id map\n5.Delete Id map\n6.Show Id number mapping\n7.Initialize database\n8.Exit\nplease choose an option:"))
         if choice > 0 and choice < 9:
@@ -80,6 +60,7 @@ try:
                 for row in c.fetchall():
                     print(row[0],row[1])
             elif choice == 7:
+                
                 c.execute("CREATE TABLE rooms(id INT NOT NULL AUTO_INCREMENT,room VARCHAR(10) NOT NULL,\
                           password VARCHAR(20) NOT NULL,PRIMARY KEY (id),UNIQUE KEY (room))")
                 c.execute("CREATE TABLE session_code(code VARCHAR(32) NOT NULL)")
