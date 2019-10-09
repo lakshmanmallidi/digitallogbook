537eaafd567310be8e619ddbdbe7acda5445e8c2
lakshman mallidi <lakshman.mallidi@gmail.com>
2019-10-09 11:51:48 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
 import mysql.connector
 
+host = '127.0.0.1'
+user = 'root'
+passwd = 'your_password'
+db = 'logbook'
 def connection():
-    conn = mysql.connector.connect(host='127.0.0.1',
-                                   user='root',
-                                   passwd='mydatabase123!',
-                                   db='logbook')
+    conn = mysql.connector.connect(host=host,
+                                   user=user,
+                                   passwd=passwd,
+                                   db=db)
     c = conn.cursor()
     return c, conn
